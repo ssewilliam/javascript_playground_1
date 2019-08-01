d0abd8b55a1f6f88333a74352c9d16ff90be014a
william sserubiri <deriwilliams2008@gmail.com>
2019-08-01 23:14:26 +0300

    ch(setup): set up a blank react app

---
+++ src/containers/login/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render(){
+    return(
+      <div>
+        <form></form>
+      </div>
+    );
+  }
+}
+export default Login
